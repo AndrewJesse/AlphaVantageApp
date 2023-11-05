d1b24a33fe5df3513b2780d4e8555e133c8bf583
Andrew J Marchese <aj.marchese@outlook.com>
2023-11-04 22:39:58 -0700

    added json class

--- Program.cs
+++ Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace ConsoleTests
+{
+    internal class Program
+    {
+        private static async Task Main(string[] args)
+        {
+            string QUERY_URL = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=IBM&apikey=demo";
+
+            using (HttpClient client = new HttpClient())
+            {
+                // Use GetStringAsync and await its result.
+                string? jsonString = await client.GetStringAsync(QUERY_URL);
+
+                // Specify the correct namespace if both Newtonsoft.Json and System.Text.Json are referenced.
+                dynamic? json_data = System.Text.Json.JsonSerializer.Deserialize<Dictionary<string, dynamic>>(jsonString);
+
+                // do something with the json_data
+            }
+        }
+    }
+}

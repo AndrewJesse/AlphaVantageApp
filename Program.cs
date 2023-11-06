fe567a4cfd2ce999d0330ebf7899638643cca540
Andrew J Marchese <aj.marchese@outlook.com>
2023-11-05 21:31:24 -0800

    defined classes in the structure of api data

--- Program.cs
+++ Program.cs
@@ -1,34 +1,80 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Net.Http;
+using System.Reflection.Metadata.Ecma335;
 using System.Text.Json;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
+using System.Linq;
 
-namespace ConsoleTests
+string keyPath = "api_key.json";
+string json = File.ReadAllText(keyPath);
+var jsonObject = JsonSerializer.Deserialize<GetAPI>(json);
+string QUERY_URL = $"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=IBM&apikey={jsonObject?.api_key}";
+
+using (HttpClient client = new HttpClient())
 {
-    internal class Program
+    // Use GetStringAsync and await its result.
+    string? jsonString = await client.GetStringAsync(QUERY_URL);
+
+    // Deserialize the JSON response into a StockData object.
+    var stockData = JsonSerializer.Deserialize<StockData>(jsonString);
+
+    // Loop through the daily quotes and print out the closing price for each day.
+    if (stockData != null && stockData.TimeSeriesDaily != null)
     {
-        private static async Task Main(string[] args)
+        foreach (var entry in stockData.TimeSeriesDaily.Take(20))
         {
-            string keyPath = "api_key.json";
-            string json = File.ReadAllText(keyPath);
-            var jsonObject = JsonSerializer.Deserialize<GetAPI>(json);
-            string QUERY_URL = $"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=IBM&apikey={jsonObject?.api_key}";
-
-            using (HttpClient client = new HttpClient())
-            {
-                // Use GetStringAsync and await its result.
-                string? jsonString = await client.GetStringAsync(QUERY_URL);
-
-                // Specify the correct namespace if both Newtonsoft.Json and System.Text.Json are referenced.
-                dynamic? json_data = System.Text.Json.JsonSerializer.Deserialize<Dictionary<string, dynamic>>(jsonString);
-
-                Console.WriteLine(jsonObject?.api_key);
-            }
+            // Process the item here
+            Console.WriteLine($"Closing price for {entry.Key}: {entry.Value.Close}");
         }
     }
-    public class GetAPI
-    {
-        public string? api_key { get; set; }
-    }
+}
+
+public class GetAPI
+{
+    public string? api_key { get; set; }
+}
+public class StockData
+{
+    [JsonPropertyName("Meta Data")]
+    public MetaData? MetaData { get; set; }
+
+    [JsonPropertyName("Time Series (Daily)")]
+    public Dictionary<string, DailyQuote>? TimeSeriesDaily { get; set; }
+}
+public class MetaData
+{
+    [JsonPropertyName("1. Information")]
+    public string? Information { get; set; }
+
+    [JsonPropertyName("2. Symbol")]
+    public string? Symbol { get; set; }
+
+    [JsonPropertyName("3. Last Refreshed")]
+    public string? LastRefreshed { get; set; }
+
+    [JsonPropertyName("4. Output Size")]
+    public string? OutputSize { get; set; }
+
+    [JsonPropertyName("5. Time Zone")]
+    public string? TimeZone { get; set; }
+}
+
+public class DailyQuote
+{
+    [JsonPropertyName("1. open")]
+    public string? Open { get; set; }
+
+    [JsonPropertyName("2. high")]
+    public string? High { get; set; }
+
+    [JsonPropertyName("3. low")]
+    public string? Low { get; set; }
+
+    [JsonPropertyName("4. close")]
+    public string? Close { get; set; }
+
+    [JsonPropertyName("5. volume")]
+    public string? Volume { get; set; }
 }
